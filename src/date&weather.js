9c720e49a822e9d22017ebb26dc1af6b336d5345
Olive Phan <108431343+yazziewonphon@users.noreply.github.com>
2022-10-08 19:55:59 -0500

    changed index to 5 from 6

--- src/date&weather.js
+++ src/date&weather.js
@@ -72,7 +72,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 5) {
     forecastHTML =
       forecastHTML +
       `
