028b313d947753cc42dee1d6ae8d5b7b4ce14e78
Olive Phan <108431343+yazziewonphon@users.noreply.github.com>
2022-10-09 12:54:20 -0500

    adjusting collision detection

--- src/date&weather.js
+++ src/date&weather.js
@@ -233,7 +233,7 @@ let isAlive = setInterval(function () {
   console.log("cactusLeft");
 
   //Detect Collision
-  if (cactusLeft < 24 && cactusLeft > 0 && ghostTop <= 39) {
+  if (cactusLeft < 75 && cactusLeft > 0 && ghostTop <= 34) {
     alert("Game Over!");
   }
 }, 10);
